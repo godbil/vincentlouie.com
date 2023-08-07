cd46437dc59a82701069f44fe28c397ca175ac73
Vincent Louie <vincentsciguy@gmail.com>
2023-08-07 19:52:37 -0400

    Minor Improvements
    Fixed issue where images were going through navbar and added scrolling effect for navbar buttons.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import email from '../assets/email-outline.svg'
 import git from '../assets/github.svg'
 import linkedin from '../assets/linkedin.svg'
+import arrow from '../assets/arrow-down.svg'
 
 function Home() {
     return (
+        <>
         <div className="top" id="top">
             <div className="name">Vincent Louie</div>
             <div className="break"></div>
@@ -13,6 +15,10 @@ function Home() {
             <a href="https://github.com/godbil"><img src={git} alt="GitHub"/></a>
             <a href="https://www.linkedin.com/in/vincent-louie-66447a224/"><img src={linkedin} alt="LinkedIn"/></a>
         </div>
+        <div className='arrow'>
+            <a className='arrow' href="#about"><img src={arrow} alt="Arrow"/></a>
+        </div>
+    </>
     );
 }
 
