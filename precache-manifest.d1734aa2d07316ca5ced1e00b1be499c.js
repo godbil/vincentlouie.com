9d4391aabaa8b96454570d19ee6ee8754ebb8fad
Vincent Louie <vincentsciguy@gmail.com>
2023-08-11 12:49:56 -0400

    Updates

---
+++ precache-manifest.d1734aa2d07316ca5ced1e00b1be499c.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "45c86f85eda0be2ee015",
+    "url": "/vincentlouie.com/static/css/main.fb841090.chunk.css"
+  },
+  {
+    "revision": "45c86f85eda0be2ee015",
+    "url": "/vincentlouie.com/static/js/main.4a66fe0b.chunk.js"
+  },
+  {
+    "revision": "0cad31b7a85fbdda87be",
+    "url": "/vincentlouie.com/static/js/runtime~main.e2312ac2.js"
+  },
+  {
+    "revision": "26e95f892246d3718344",
+    "url": "/vincentlouie.com/static/js/2.46d129cb.chunk.js"
+  },
+  {
+    "revision": "efb71002a0fdbb4113c1",
+    "url": "/vincentlouie.com/static/js/3.e513e70d.chunk.js"
+  },
+  {
+    "revision": "718891a8faf8dea0c5e504c2688b4aa8",
+    "url": "/vincentlouie.com/static/media/github.718891a8.svg"
+  },
+  {
+    "revision": "a3171f2257fb370d153861de78396297",
+    "url": "/vincentlouie.com/static/media/email-outline.a3171f22.svg"
+  },
+  {
+    "revision": "0917c1fc4d92dba29242174977c7b613",
+    "url": "/vincentlouie.com/static/media/linkedin.0917c1fc.svg"
+  },
+  {
+    "revision": "74514a31a36e1ade6482a20a3d1d51ef",
+    "url": "/vincentlouie.com/static/media/arrow-down.74514a31.svg"
+  },
+  {
+    "revision": "4748f0d6f8874ed17ceefd20dab25de4",
+    "url": "/vincentlouie.com/static/media/mtd-menu.4748f0d6.png"
+  },
+  {
+    "revision": "c7ad5378b3d0cb31bcf82b38da617e7c",
+    "url": "/vincentlouie.com/static/media/consoless.c7ad5378.png"
+  },
+  {
+    "revision": "05a38cb8c39abfc05a03594e46347fad",
+    "url": "/vincentlouie.com/static/media/cardgame.05a38cb8.png"
+  },
+  {
+    "revision": "be8b93210010142ff0b2c8884bdf7ae8",
+    "url": "/vincentlouie.com/static/media/site.be8b9321.png"
+  },
+  {
+    "revision": "059208a28da7e2fd8400cb65b35c8bca",
+    "url": "/vincentlouie.com/static/media/web.059208a2.svg"
+  },
+  {
+    "revision": "e8d408c88538a9b83768d5fc8d584bbf",
+    "url": "/vincentlouie.com/static/media/louievi1.e8d408c8.pdf"
+  },
+  {
+    "revision": "d8c6b68b6f532ac248969460c08d1dab",
+    "url": "/vincentlouie.com/index.html"
+  }
+];
